a3a93118778fae3e3574f01b8ce29cf805ce826e
João Vinicius <joviusdsg@gmail.com>
2024-10-31 20:07:11 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/atividade_final_progii/Atividade_final_progII.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade_final_progii;
+
+/**
+ *
+ * @author joaov
+ */
+public class Atividade_final_progII {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
